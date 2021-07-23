ae33913814b65065b608a24177b1112075f10589
Finx <86189855+SedatTR3348237878HMLD@users.noreply.github.com>
2021-07-23 16:46:49 -0400

    Add files via upload

---
+++ guncelle.sh
@@ -0,0 +1,39 @@
+pkg install git -y
+pkg install python python2 -y
+pkg install wget -y
+pkg install curl -y
+pkg install ruby -y
+pkg install php -y
+pkg install pip pip2 -y
+pkg install clang -y
+pkg install vim -y
+pkg install nano -y
+apt install proot -y
+pkg install cat -y
+pkg install figlet -y
+pkg install cmatrix -y
+pkg install perl -y
+apt update
+apt upgrade -y
+apt install git -y
+apt install python python2 -y
+apt install wget -y
+apt install curl -y
+apt install ruby -y
+apt install php -y
+apt install pip pip2 -y
+apt install clang -y
+apt install vim -y
+apt install nano -y
+apt install proot -y
+apt install cat -y
+apt install figlet -y
+apt install cmatrix -y
+apt install perl -y
+apt install python2 -y
+apt install python3 -y
+
+clear
+echo -e '\033[31;40;1m              Kurulum Bitti!'
+sleep 2
+bash menu.sh
---
+++ menu.sh
@@ -0,0 +1,3 @@
+echo -e "\033[1m \e[32m (Merhaba Termuxunuz Veya Linuxunuz Başarıyla Gerekli Programlar Kurulmuştur Ve Güncelleştirilmiştir. Selamlar! )"
+
+figlet Kurulum Tamam BB | lolcat
